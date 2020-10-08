74645cf7ae34fcf6788287a56e90d1a7080d0548
Philipp Schlesinger <info@philcomm.dev>
2020-10-08 19:25:32 +0200

    Add uninstall script

---
+++ uninstall.sh
@@ -0,0 +1,16 @@
+# Make sure script is executed as sudo
+if [[ $EUID -ne 0 ]]; then
+   echo "Please execute script as root." 
+   exit 1
+fi
+
+# Stop and remove service
+service=/etc/systemd/system/retropie_background_music.service
+sudo systemctl disable retropie_background_music
+sudo systemctl stop retropie_background_music
+rm $service
+
+# Delete python3 script
+script=/opt/dev_philcomm/music.py
+rm $script
+rmdir /opt/dev_philcomm
